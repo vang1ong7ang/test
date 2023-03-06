00c27a369b79d4e2dbe9a87edf2af72c85a9bdb8
vang1ong7ang <vang1ong7ang@outlook.com>
2023-03-06 06:03:13 -0500

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,2 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js') ?? workbox.routing.registerRoute(new RegExp('.*\.*'), new workbox.strategies.NetworkFirst())
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
+workbox.routing.registerRoute(new RegExp('.*\.*'), new workbox.strategies.NetworkFirst());
